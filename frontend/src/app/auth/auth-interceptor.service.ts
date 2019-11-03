908d6727ada5e34780e1650edecbb1b8e3725dff
Haris Beslic <halcika_7@hotmail.com>
2019-11-03 19:43:28 +0100

    landing_page - improved interceptor

--- frontend/src/app/auth/auth-interceptor.service.ts
+++ frontend/src/app/auth/auth-interceptor.service.ts
@@ -1,29 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
-import { take, exhaustMap, map } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
-
-import * as fromApp from '../store/app.reducer';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler
+} from '@angular/common/http';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor() {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    return this.store.select('auth').pipe(
-      take(1),
-      map(authState => {
-        return authState.accessToken;
-      }),
-      exhaustMap(token => {
-        const url = 'http://localhost:5000/api';
-        const modifiedReq = req.clone({
-          url: url + req.url,
-          headers: req.headers.set('Authorization', 'Bearer ' + token),
-          withCredentials: true
-        });
-        return next.handle(modifiedReq);
-      })
-    );
+    const token = localStorage.getItem('accessToken')
+      ? localStorage.getItem('accessToken')
+      : sessionStorage.getItem('accessToken');
+    const url = 'http://localhost:5000/api';
+    const modifiedReq = req.clone({
+      url: url + req.url,
+      headers: req.headers.set('Authorization', 'Bearer ' + token),
+      withCredentials: true
+    });
+    return next.handle(modifiedReq);
   }
 }
