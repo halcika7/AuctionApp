cf9f78ceda4f2128037c1fd4fe5b169bb0303473
Haris Beslic <halcika_7@hotmail.com>
2019-12-29 02:10:53 +0100

    notifications - checking date when user adds new product on deployed app

--- backend/helpers/transformProductData.js
+++ backend/helpers/transformProductData.js
@@ -11,9 +11,13 @@ module.exports.transformProductData = (
   productData.brandId = brandData.id;
   productData.userId = userId;
   productData.auctionStart = new Date(productData.startDate);
+  console.log('TCL: productData.startDate', productData.startDate)
   productData.auctionStart.setHours(1, 0, 0, 0);
+  productData.auctionStart.toUTCString();
   productData.auctionEnd = new Date(productData.endDate);
+  console.log('TCL: productData.endDate', productData.endDate)
   productData.auctionEnd.setHours(0, 0, 0, 0);
+  productData.auctionEnd.toUTCString();
   
   delete productData.description;
   delete productData.endDate;
