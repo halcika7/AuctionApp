d2a42ab476467bc135107b5795dbe15732b5c116
Haris Beslic <halcika_7@hotmail.com>
2019-12-29 01:02:23 +0100

    notifications - reset all to 00 for heroku deployment

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -9,16 +9,14 @@ const { findProductsByAuctionEnd } = require('../helpers/productFilter');
 module.exports = io => {
   io.on('connection', socket => {
     new CronJob(
-      '00 */1 * * * *',
+      '00 00 00 * * *',
       async () => {
         try {
           const date = new Date();
-          date.setHours(24, 0, 0, 0);
-          console.log('TCL: date', date)
+          date.setHours(0, 0, 0, 0);
 
           const products = await findProductsByAuctionEnd(date);
           const productIds = products.map(product => product.id);
-          console.log('TCL: productIds', productIds)
 
           await WishlistService.removeFromWishlistByProductId(productIds);
 
