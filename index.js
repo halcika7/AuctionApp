536744051ae4236a3aa40f224e21d16df9dcb5c4
Haris Beslic <halcika_7@hotmail.com>
2020-01-06 01:50:07 +0100

    final tray for deployment

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+      if (req.headers['x-forwarded-proto'] !== 'https')
+          return res.redirect('https://' + req.headers.host + req.url);
+      else
+          return next();
+  } else
+      return next();
+});
+
 require('./backend/config/database');
 require('./backend/models/Associations');
 
