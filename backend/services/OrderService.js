0d32badfad97aa59a9ee29eaa49ef735eecc4a02
Haris Beslic <halcika_7@hotmail.com>
2020-01-07 00:13:16 +0100

    ckeck for error

--- backend/services/OrderService.js
+++ backend/services/OrderService.js
@@ -69,6 +69,7 @@ class OrderService extends BaseService {
         ownerId: userId
       });
     } catch (error) {
+      console.log('TCL: updateOrder -> error', error)
       return super.returnResponse(403, {
         message: 'Something happened. We were unable to perform request.'
       });
